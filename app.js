4577a09ebb3170cc3af28f5e6fe3056da594fe3c
manumastro <emanuele.mastronardi.2002.12@gmail.com>
2022-07-29 12:33:54 +0200

    added a preloader

--- app.js
+++ app.js
@@ -15,3 +15,9 @@ btn.addEventListener('click', function(){
     video.play();
   }
 });
+
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', function(){
+  preloader.classList.add("hide-preloader");
+});
